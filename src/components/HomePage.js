d6298f611f36a7d63934565fb3a75f3382e303a9
HungNguyen-89 <hungnguyenhuts89@gmail.com>
2023-11-05 12:00:37 +0700

    update dict hompage 2023-11-05#001

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,7 +5,7 @@ const HomPage = () => {
   return (
     <>
       <div className="hompage-container">
-        <div className="dictionary-content">DICTIONARY</div>
+        <div className="dictionary-title">DICTIONARY</div>
         <div className="dictionary-box-search">
           <div className="dictionary-box-icon">
             <img alt="" src="https://i.imgur.com/1r8PmNM.png" />
@@ -13,7 +13,7 @@ const HomPage = () => {
           <input type="text" placeholder="Type here to search" />
           <button className="dictionary-box-search-btn">Search</button>
         </div>
-        <div className="hompage-category-container">
+        {/* <div className="hompage-category-container">
           <div className="hompage-category">
             <div className="hompage-category-icon">
               <img alt="" src="https://i.imgur.com/a5yyEF7.png" />
@@ -48,6 +48,25 @@ const HomPage = () => {
             <div className="hompage-category-title">
               <div className="hompage-category-title-up">Funny</div>
               <div className="hompage-category-title-down">Game</div>
+            </div>
+          </div>
+        </div> */}
+        <div className="dictionary-content">
+          <div className="words-to-search-for">run</div>
+          <div className="word-classification">verb</div>
+          <div className="word-phonetic">/ran/</div>
+          <div className="meaning-of-the-word-container">
+            <div className="meaning-of-the-word-en">
+              (of a person or animal) to move quickly, faster than walking
+            </div>
+            <div className="meaning-of-the-word-vn">chạy</div>
+            <div className="example-of-the-word-container">
+              <div className="example-of-the-word-container-en">
+                He ran down the road.
+              </div>
+              <div className="example-of-the-word-container-vn">
+                Anh ấy chạy xuống đường.
+              </div>
             </div>
           </div>
         </div>
