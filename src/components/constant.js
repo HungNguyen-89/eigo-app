d18aee08df40d0dbd7808edf629451bd9fcafea2
HungNguyen-89 <hungnguyenhuts89@gmail.com>
2025-03-16 00:01:04 +0700

    update grammar page 2025-03-16 ver001

--- src/components/constant.js
+++ src/components/constant.js
@@ -10,7 +10,7 @@ export const routes = [
   //   subMenu: false,
   // },
   {
-    name: "Vocabulary (Translate sentences)",
+    name: "Vocabulary",
     link: "/",
     subMenu: true,
     subRoutes: [
